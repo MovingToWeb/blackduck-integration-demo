4ba0119c1e11686b97351c8964ba3d341da81a76
Ajay Kumar <apple@apples-MacBook-Pro.local>
2019-04-30 11:46:59 +0530

    Add initial files for blackduck integration demo

---
+++ src/main/java/com/example/blackduckintegrationdemo/BlackduckIntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.blackduckintegrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackduckIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackduckIntegrationDemoApplication.class, args);
+	}
+
+}
