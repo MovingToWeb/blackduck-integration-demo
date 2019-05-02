a426a2501fc82f7742991667dc62c50b44f5768b
Ajay Kumar <apple@apples-MacBook-Pro.local>
2019-05-02 17:30:39 +0530

    correct deployment scripts

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Trying to Stop the Server (if its running)"
 if [ -d "/deployment/pid.file" ]; then
-    pkill $(cat /deployment/pid.file);
+    pkill $(cat /deployment/pid.file)
     echo "Killed process at: "$(cat /deployment/pid.file)
 fi
