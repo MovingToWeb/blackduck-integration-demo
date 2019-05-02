b6df2c110440e9ce8175408089047194a32ff1da
Ajay Kumar <apple@apples-MacBook-Pro.local>
2019-05-02 08:51:36 +0530

    add filepaths

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Changing permissions for /deployment folder..."
-chmod +x /deployment
-echo "Created /deployment folder"
+chmod -R 755 /deployment
+echo "Changed /deployment folder permissions"
