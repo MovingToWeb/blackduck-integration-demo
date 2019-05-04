1b95afdea4db2c781c61d54448ccda07a2f0657b
Ajay Kumar <apple@apples-MacBook-Pro.local>
2019-05-04 07:27:43 +0530

    add stop statement

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,5 +3,5 @@ echo "Trying to Stop the Server (if its running)"
 pid=`ps aux | grep "blackduck" | grep -v 'grep' | awk '{print $2}'`
 echo "Running PID: "$pid
 sudo kill $pid
-echo $(ps aux | grep "blackduck" | grep -v 'grep' | awk '{print $2}')
+echo "Running PID: "$(ps aux | grep "blackduck" | grep -v 'grep' | awk '{print $2}')
 echo "Killed process."
