c2a2437ef35b0a59f8331cdcacc9a95e1aee12fa
Ajay Kumar <apple@apples-MacBook-Pro.local>
2019-05-04 07:05:58 +0530

    add stop statement

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Trying to Stop the Server (if its running)"
-if [ -d "/deployment/pid.file" ]; then
-    pkill $(cat /deployment/pid.file)
-    echo "Killed process at: "$(cat /deployment/pid.file)
+pid=`ps aux | grep "blackduck" | grep -v 'grep' | awk '{print $2}'`
+echo "Running PID: "$pid
+sudo pkill $pid
+echo "Killed process."
 fi
